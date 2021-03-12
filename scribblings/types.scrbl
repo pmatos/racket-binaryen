3535a9400b20f5e64d799bb70916d85466802e6d
Paulo Matos <pmatos@linki.tools>
2021-03-12 11:46:37 +0100

    Add remaining types and documentation

--- scribblings/types.scrbl
+++ scribblings/types.scrbl
@@ -15,3 +15,37 @@ Types refer to WebAssembly types such as @tt{int32}, @tt{int64}, etc. There are 
 
 @defthing[type-int64 type?]{
  The type @tt{Int64}.}
+
+@defthing[type-float32 type?]{
+ The type @tt{Float32}.}
+
+@defthing[type-float64 type?]{
+ The type @tt{Float64}.}
+
+@defthing[type-vec128 type?]{
+ The type @tt{Vec128}.}
+
+@defthing[type-funcref type?]{
+ The type @tt{Funcref}.}
+
+@defthing[type-externref type?]{
+ The type @tt{Externref}.}
+
+@defthing[type-anyref type?]{
+ The type @tt{Anyref}.}
+
+@defthing[type-eqref type?]{
+ The type @tt{Eqref}.}
+
+@defthing[type-i31ref type?]{
+ The type @tt{I31ref}.}
+
+@defthing[type-dataref type?]{
+ The type @tt{Dataref}.}
+
+@defthing[type-unreachable type?]{
+ The type @tt{Unreachable}.}
+
+@defthing[type-auto type?]{
+ The type @tt{Auto}.}
+
--- types.rkt
+++ types.rkt
@@ -8,13 +8,35 @@
 (provide
  type-none
  type-int32
- type-int64)
+ type-int64
+ type-float32
+ type-float64
+ type-vec128
+ type-funcref
+ type-externref
+ type-anyref
+ type-eqref
+ type-i31ref
+ type-dataref
+ type-unreachable
+ type-auto)
 
 ;; ---------------------------------------------------------------------------------------------------
 
 (define type-none (type (BinaryenTypeNone)))
 (define type-int32 (type (BinaryenTypeInt32)))
 (define type-int64 (type (BinaryenTypeInt64)))
+(define type-float32 (type (BinaryenTypeFloat32)))
+(define type-float64 (type (BinaryenTypeFloat64)))
+(define type-vec128 (type (BinaryenTypeVec128)))
+(define type-funcref (type (BinaryenTypeFuncref)))
+(define type-externref (type (BinaryenTypeExternref)))
+(define type-anyref (type (BinaryenTypeAnyref)))
+(define type-eqref (type (BinaryenTypeEqref)))
+(define type-i31ref (type (BinaryenTypeI31ref)))
+(define type-dataref (type (BinaryenTypeDataref)))
+(define type-unreachable (type (BinaryenTypeUnreachable)))
+(define type-auto (type (BinaryenTypeAuto)))
 
 ;; ---------------------------------------------------------------------------------------------------
 
